d1b86bebb5bd7ef7674fcf215864748fe8f733fd
Peter Szücs <pepo_szucs@hotmail.com>
2021-03-10 22:41:27 +0100

    Initial Commit

---
+++ MyPlace/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyPlace
+//
+//  Created by Peter Szücs on 2021-03-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyPlace/MyPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPlaceApp.swift
+//  MyPlace
+//
+//  Created by Peter Szücs on 2021-03-10.
+//
+
+import SwiftUI
+
+@main
+struct MyPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
