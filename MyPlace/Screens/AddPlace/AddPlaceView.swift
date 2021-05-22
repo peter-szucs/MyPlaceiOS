71fe5e05f88cd1c49aa2bbba84cb66c1ce4a5ec1
Peter Szücs <pepo_szucs@hotmail.com>
2021-05-22 18:16:37 +0200

    Fixed Keyboard pushing top content out of sight in Add Place (wrapping all in scrollview). Now compatible with lower iPhone models.

--- MyPlace/Screens/AddPlace/AddPlaceView.swift
+++ MyPlace/Screens/AddPlace/AddPlaceView.swift
@@ -15,109 +15,111 @@ struct AddPlaceView: View {
     
     var body: some View {
         
-        VStack {
-            if viewModel.isLoading {
-                ActivityView()
-            } else {
-                VStack(alignment: .leading) {
-                    
-                    FormTextView(text: LocalizedStringKey("AddPlace_name"))
-                    TextField(LocalizedStringKey("AddPlace_namePH"), text: $viewModel.place.title)
-                        .modifier(TextFieldClearButton(text: $viewModel.place.title))
-                        .padding([.leading, .trailing])
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .keyboardType(.alphabet)
-                        .disableAutocorrection(true)
-                    
+        ScrollView {
+            VStack {
+                if viewModel.isLoading {
+                    ActivityView()
+                } else {
                     VStack(alignment: .leading) {
-                        Text(viewModel.place.PMData.PMName)
-                        HStack {
-                            Text("\(viewModel.place.PMData.PMAddress) \(viewModel.place.PMData.PMAddress2)")
-                        }
-                        HStack {
-                            Text(viewModel.place.PMData.PMZipCode)
-                            Text(viewModel.place.PMData.PMNeighbourhood)
-                        }
-                        Text(viewModel.place.PMData.PMState)
-                        Text(viewModel.place.PMData.PMCountry)
-                    }
-                    .font(.footnote)
-                    .padding(.horizontal)
-                    
-                    FormTextView(text: LocalizedStringKey("AddPlace_description"))
-                    TextEditor(text: $viewModel.place.description)
-                        .frame(width: UIScreen.main.bounds.width - 40, height: 150)
-                        .multilineTextAlignment(.leading)
-                        .padding(.horizontal, 5)
-                        .border(Color("TextEditorBorderColor"), width: 1)
-                        .cornerRadius(3)
-                        .padding(.horizontal)
-                    
-                    VStack(alignment: .leading) {
-                        FormTextView(text: LocalizedStringKey("AddPlace_tags"))
-                        AddPlaceTagsView(tags: viewModel.place.tags)
-                            .padding(.horizontal)
-                        Button(action: {
-                            viewModel.activeSheet = .first
-                        }, label: {
-                            AddPlaceAddTagsView()
-                                .padding(.horizontal, 10)
-                                .accentColor(.gray)
-                        })
-                    }
-                    
-                    ScrollView(.horizontal) {
+                        
+                        FormTextView(text: LocalizedStringKey("AddPlace_name"))
+                        TextField(LocalizedStringKey("AddPlace_namePH"), text: $viewModel.place.title)
+                            .modifier(TextFieldClearButton(text: $viewModel.place.title))
+                            .padding([.leading, .trailing])
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .keyboardType(.alphabet)
+                            .disableAutocorrection(true)
+                        
                         VStack(alignment: .leading) {
-                            HStack(spacing: 20) {
-                                ForEach(viewModel.images, id:\.self) { image in
-                                    AddedPhotosView(image: Image(uiImage: image), type: .pickedImage, showPicker: $viewModel.activeSheet)
+                            Text(viewModel.place.PMData.PMName)
+                            HStack {
+                                Text("\(viewModel.place.PMData.PMAddress) \(viewModel.place.PMData.PMAddress2)")
+                            }
+                            HStack {
+                                Text(viewModel.place.PMData.PMZipCode)
+                                Text(viewModel.place.PMData.PMNeighbourhood)
+                            }
+                            Text(viewModel.place.PMData.PMState)
+                            Text(viewModel.place.PMData.PMCountry)
+                        }
+                        .font(.footnote)
+                        .padding(.horizontal)
+                        
+                        FormTextView(text: LocalizedStringKey("AddPlace_description"))
+                        TextEditor(text: $viewModel.place.description)
+                            .frame(width: UIScreen.main.bounds.width - 40, height: 150)
+                            .multilineTextAlignment(.leading)
+                            .padding(.horizontal, 5)
+                            .border(Color("TextEditorBorderColor"), width: 1)
+                            .cornerRadius(3)
+                            .padding(.horizontal)
+                        
+                        VStack(alignment: .leading) {
+                            FormTextView(text: LocalizedStringKey("AddPlace_tags"))
+                            AddPlaceTagsView(tags: viewModel.place.tags)
+                                .padding(.horizontal)
+                            Button(action: {
+                                viewModel.activeSheet = .first
+                            }, label: {
+                                AddPlaceAddTagsView()
+                                    .padding(.horizontal, 10)
+                                    .accentColor(.gray)
+                            })
+                        }
+                        
+                        ScrollView(.horizontal) {
+                            VStack(alignment: .leading) {
+                                HStack(spacing: 20) {
+                                    ForEach(viewModel.images, id:\.self) { image in
+                                        AddedPhotosView(image: Image(uiImage: image), type: .pickedImage, showPicker: $viewModel.activeSheet)
+                                    }
+                                    AddedPhotosView(image: nil, type: .addImage, showPicker: $viewModel.activeSheet)
                                 }
-                                AddedPhotosView(image: nil, type: .addImage, showPicker: $viewModel.activeSheet)
+                            }
+                            .padding(.horizontal, 10)
+                        }
+                        .frame(maxWidth: UIScreen.main.bounds.width - 20, minHeight: 80, maxHeight: 100, alignment: .center)
+                        Text(viewModel.emptyFieldsMessage)
+                            .foregroundColor(.red)
+                            .padding(.horizontal)
+                        Spacer()
+                    }
+                }
+            }
+            .navigationBarTitle(LocalizedStringKey("AddPlace_title"), displayMode: .inline)
+            .toolbar(content: {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        hideKeyboard()
+                    }, label: {
+                        Image(systemName: "keyboard.chevron.compact.down")
+                    })
+                    Button(action: {
+                        print("!!! \(viewModel.place)")
+                        viewModel.finalizePlace { (success) in
+                            if success {
+                                presentationMode.wrappedValue.dismiss()
                             }
                         }
-                        .padding(.horizontal, 10)
-                    }
-                    .frame(maxWidth: UIScreen.main.bounds.width - 20, minHeight: 80, maxHeight: 100, alignment: .center)
-                    Text(viewModel.emptyFieldsMessage)
-                        .foregroundColor(.red)
-                        .padding(.horizontal)
-                    Spacer()
+                    }, label: {
+                        Text(LocalizedStringKey("Save"))
+                    })
                 }
                 
+            })
+            .sheet(item: $viewModel.activeSheet) { item in
+                switch item {
+                case .first:
+                    AddTagsView(viewModel: viewModel)
+                case .second:
+                    ImagePicker(image: $viewModel.pickedImage, sourceType: .camera).onDisappear { viewModel.loadImages() }
+                }
             }
+            .onAppear {
+                viewModel.place = self.place
         }
-        .navigationBarTitle(LocalizedStringKey("AddPlace_title"), displayMode: .inline)
-        .toolbar(content: {
-            ToolbarItemGroup(placement: .navigationBarTrailing) {
-                Button(action: {
-                    hideKeyboard()
-                }, label: {
-                    Image(systemName: "keyboard.chevron.compact.down")
-                })
-                Button(action: {
-                    print("!!! \(viewModel.place)")
-                    viewModel.finalizePlace { (success) in
-                        if success {
-                            presentationMode.wrappedValue.dismiss()
-                        }
-                    }
-                }, label: {
-                    Text(LocalizedStringKey("Save"))
-                })
-            }
-            
-        })
-        .sheet(item: $viewModel.activeSheet) { item in
-            switch item {
-            case .first:
-                AddTagsView(viewModel: viewModel)
-            case .second:
-                ImagePicker(image: $viewModel.pickedImage, sourceType: .camera).onDisappear { viewModel.loadImages() }
-            }
         }
-        .onAppear {
-            viewModel.place = self.place
-        }
+//        .ignoresSafeArea(.keyboard, edges: .bottom)
     }
 }
 
